7a77472971f2d6cd3853d187fc181bacb6d716a6
antasofa7 <antasofa.dev@gmail.com>
2024-01-27 23:42:08 +0700

    Init project and Update dependencies.

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
@@ -12,38 +10,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       restorationScopeId: 'app',
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en', ''), // English, no country code
-      ],
-      onGenerateTitle: (BuildContext context) =>
-          AppLocalizations.of(context)!.appTitle,
-
       theme: ThemeData(useMaterial3: true),
       darkTheme: ThemeData.dark(useMaterial3: true),
-      // themeMode: settingsController.themeMode,
-
-      // onGenerateRoute: (RouteSettings routeSettings) {
-      //   return MaterialPageRoute<void>(
-      //     settings: routeSettings,
-      //     builder: (BuildContext context) {
-      //       switch (routeSettings.name) {
-      //         case SettingsView.routeName:
-      //           return SettingsView(controller: settingsController);
-      //         case SampleItemDetailsView.routeName:
-      //           return const SampleItemDetailsView();
-      //         case SampleItemListView.routeName:
-      //         default:
-      //           return const SampleItemListView();
-      //       }
-      //     },
-      //   );
-      // },
     );
   }
 }
